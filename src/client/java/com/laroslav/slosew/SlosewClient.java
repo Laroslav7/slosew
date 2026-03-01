b99996b7a8673c055a0bd63d3a68b3223afa02d9
Laroslav7 <microsoftjfdjg7@gmail.com>
2026-03-01 22:27:21 +0200

    first commit

---
+++ src/client/java/com/laroslav/slosew/SlosewClient.java
@@ -0,0 +1,10 @@
+package com.laroslav.slosew;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SlosewClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
